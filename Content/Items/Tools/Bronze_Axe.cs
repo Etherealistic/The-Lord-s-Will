45bd6ed58b8b463fb862e7501bb26e7cd90fa447
Etherealistic <franticfranx@gmail.com>
2025-08-20 12:09:04 +0200

    Added axe and fixed bronze sword texture

---
+++ Content/Items/Tools/Bronze_Axe.cs
@@ -0,0 +1,34 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheLordsWill.Content.Items.Tools;
+
+public class Bronze_Axe : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.damage = 7;
+        Item.DamageType = DamageClass.Melee;
+        Item.width = 50;
+        Item.height = 40;
+        Item.useTime = 18;
+        Item.useAnimation = 20;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.knockBack = 4.5f;
+        Item.value = Item.buyPrice(silver: 15);
+        Item.rare = ItemRarityID.White;
+        Item.UseSound = SoundID.Item1;
+        Item.autoReuse = true;
+        Item.axe = 13;
+    }
+
+    public override void AddRecipes()
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient(ItemID.DirtBlock, 8);
+        recipe.AddIngredient(ItemID.Wood, 4);
+        recipe.AddTile(TileID.Anvils);
+        recipe.Register();
+    }
+}
